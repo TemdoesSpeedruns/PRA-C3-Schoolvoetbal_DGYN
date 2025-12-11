063ea1c39ac8c539b91f5ce82b47391bf49286eb
TemdoesSpeedruns <djpangoro@gmail.com>
2025-12-11 15:57:13 +0100

    admin dashboard heeft nu geen grijs meer
    :)

--- resources/views/AdminDashboard.blade.php
+++ resources/views/AdminDashboard.blade.php
@@ -4,43 +4,43 @@
 <div class="container-fluid">
     <div class="row mb-4">
         <div class="col-md-12">
-            <h1 class="text-black dark:text-white">Admin Dashboard</h1>
+            <h1 class="text-black">Admin Dashboard</h1>
         </div>
     </div>
 
     <div class="row">
         <div class="col-md-3">
-            <div class="card bg-white dark:bg-gray-800 border dark:border-gray-700">
+            <div class="card bg-white border">
                 <div class="card-body">
-                    <h5 class="card-title text-black dark:text-white">Total Users</h5>
-                    <p class="card-text display-4 text-black dark:text-white">{{ $totalUsers ?? 0 }}</p>
+                    <h5 class="card-title text-black">Total Users</h5>
+                    <p class="card-text display-4 text-black">{{ $totalUsers ?? 0 }}</p>
                 </div>
             </div>
         </div>
 
         <div class="col-md-3">
-            <div class="card bg-white dark:bg-gray-800 border dark:border-gray-700">
+            <div class="card bg-white border">
                 <div class="card-body">
-                    <h5 class="card-title text-black dark:text-white">Total Teams</h5>
-                    <p class="card-text display-4 text-black dark:text-white">{{ $totalTeams ?? 0 }}</p>
+                    <h5 class="card-title text-black text-white">Total Teams</h5>
+                    <p class="card-text display-4 text-black ">{{ $totalTeams ?? 0 }}</p>
                 </div>
             </div>
         </div>
 
         <div class="col-md-3">
-            <div class="card bg-white dark:bg-gray-800 border dark:border-gray-700">
+            <div class="card bg-white border">
                 <div class="card-body">
-                    <h5 class="card-title text-black dark:text-white">Total Matches</h5>
-                    <p class="card-text display-4 text-black dark:text-white">{{ $totalMatches ?? 0 }}</p>
+                    <h5 class="card-title text-black">Total Matches</h5>
+                    <p class="card-text display-4 text-black">{{ $totalMatches ?? 0 }}</p>
                 </div>
             </div>
         </div>
 
         <div class="col-md-3">
-            <div class="card bg-white dark:bg-gray-800 border dark:border-gray-700">
+            <div class="card bg-white border">
                 <div class="card-body">
-                    <h5 class="card-title text-black dark:text-white">Total Events</h5>
-                    <p class="card-text display-4 text-black dark:text-white">{{ $totalEvents ?? 0 }}</p>
+                    <h5 class="card-title text-black">Total Events</h5>
+                    <p class="card-text display-4 text-black">{{ $totalEvents ?? 0 }}</p>
                 </div>
             </div>
         </div>
@@ -48,12 +48,12 @@
 
     <div class="row mt-4">
         <div class="col-md-6">
-            <div class="card bg-white dark:bg-gray-800 border dark:border-gray-700">
-                <div class="card-header text-black dark:text-white">
+            <div class="card bg-whiteborder ">
+                <div class="card-header text-black">
                     <h5>Recent Users</h5>
                 </div>
                 <div class="card-body">
-                    <table class="table table-sm text-black dark:text-white">
+                    <table class="table table-sm text-black">
                         <thead>
                             <tr>
                                 <th>Name</th>
@@ -76,8 +76,8 @@
         </div>
 
         <div class="col-md-6">
-            <div class="card bg-white dark:bg-gray-800 border dark:border-gray-700">
-                <div class="card-header text-black dark:text-white">
+            <div class="card bg-white ">
+                <div class="card-header text-black">
                     <h5>Quick Actions</h5>
                 </div>
                 <div class="card-body">
