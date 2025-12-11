d738a06a527985017f6ce18699c07505a430e7ef
TemdoesSpeedruns <djpangoro@gmail.com>
2025-12-11 15:22:17 +0100

    uitslagen + vorige winnaars zijn nu zichtbaar
    hehehehehehhe

--- resources/views/results.blade.php
+++ resources/views/results.blade.php
@@ -1,6 +1,6 @@
 <x-app-layout>
     <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 text-black dark:text-white">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 text-black">
             <h1 class="text-3xl font-bold mb-6">Laatste Uitslagen</h1>
 
             @if($tournaments->isEmpty())
@@ -8,7 +8,7 @@
             @else
                 <ul class="space-y-4">
                     @foreach($tournaments as $tournament)
-                        <li class="border-b border-gray-300 dark:border-gray-600 pb-2">
+                        <li class="border-b border-gray-300">
                             {{ $tournament->name }} ({{ $tournament->year }})
                             @if($tournament->results)
                                 - Resultaten: {{ $tournament->results }}
